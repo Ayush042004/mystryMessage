9cd8bc9d332add37d7e28b3d2753ff666aa37f87
Ayush kumar <kumarayush042004@gmail.com>
2025-06-24 17:09:28 +0530

    feat: update dependencies and add new UI components
    - Added new dependencies: @hookform/resolvers, @radix-ui/react-label, @radix-ui/react-slot, axios, class-variance-authority, clsx, lucide-react, next-themes, react-hook-form, react-hot-toast, sonner, tailwind-merge, usehooks-ts, and tw-animate-css.
    - Implemented sign-in and sign-up pages with form validation using react-hook-form and zod.
    - Created reusable UI components: Button, Input, Form, FormField, FormLabel, FormControl, FormDescription, FormMessage, and Label.
    - Integrated a Toaster component for notifications using sonner.
    - Enhanced global styles with custom CSS variables and dark mode support.
    - Added a new components.json configuration for shadcn UI.

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,29 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
+import {useDebounceValue} from "usehooks-ts"
+import toast from "react-hot-toast"
+import { useRouter } from 'next/navigation'
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link"
+import { signInSchema } from '@/schemas/signInSchema'
 
 function page() {
+  const [username,setUsername] = useState("")
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting,setIsSubmitting] = useState(false);
+
+  const debouncedUsername = useDebounceValue(username,300)
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    
+  })
+
   return (
+
     <div>page</div>
   )
 }
