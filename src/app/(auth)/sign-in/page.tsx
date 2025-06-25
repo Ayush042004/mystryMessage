d185bd02437a7f8b475b66f1e9c44ae4f7c4b0dd
Ayush kumar <kumarayush042004@gmail.com>
2025-06-25 16:23:38 +0530

    feat: enhance sign-in page with improved form handling and UI components

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,5 @@
 'use client'
-import React, { useState } from 'react'
-import {useDebounceValue} from "usehooks-ts"
+import React, {useState } from 'react'
 import toast from "react-hot-toast"
 import { useRouter } from 'next/navigation'
 import {zodResolver} from "@hookform/resolvers/zod"
@@ -8,23 +7,98 @@ import {useForm} from "react-hook-form";
 import * as z from "zod";
 import Link from "next/link"
 import { signInSchema } from '@/schemas/signInSchema'
+import { ApiResponse } from '@/types/ApiResponse'
+import { Form, FormControl,FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Loader2 } from 'lucide-react'
+import { signIn } from 'next-auth/react'
+
 
 function page() {
-  const [username,setUsername] = useState("")
-  const [usernameMessage, setUsernameMessage] = useState("");
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmitting,setIsSubmitting] = useState(false);
+      const router = useRouter();
 
-  const debouncedUsername = useDebounceValue(username,300)
-  const router = useRouter();
+      const form = useForm<z.infer<typeof signInSchema>>({
+          resolver: zodResolver(signInSchema),
+          defaultValues: {
+            identifier:"",
+            password:"",
+          }
+        });
+        const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+          const result = await signIn('credentials', {
+            redirect: false,
+            identifier: data.identifier,
+            password: data.password,
+          });
 
-  const form = useForm<z.infer<typeof signInSchema>>({
-    
-  })
+          if(result?.error){
+            if(result.error === 'CredentialsSignin'){
+              toast('Invalid credentials. Please try again.', {
+                icon: '❌',
+              });
+            } else {
+              toast('An unexpected error occurred. Please try again later.', {
+                icon: '❌',
+              });
+            }
+          }
+          if(result?.url) {
+            router.replace('/dashboard');
+          }
+        }
+
 
   return (
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+        <div className='w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md '>
+            <div className='text-center'>
+                <h1 className='text-4xl font-extrabold tracking-tight lg:text-5xl mb-6'> welcome Back to true Feedback </h1>
+                <p className='mb-4'>Sign in to continue your secret conversations</p>
+            </div>
+         <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+               <FormField
+                 control={form.control}
+                 name="identifier"
+                 render={({field}) => (
+                    <FormItem>
+                        <FormLabel>Email/Username</FormLabel>
+                        <FormControl>
+                            <Input  {...field}/> 
+                        </FormControl>
+                        <FormMessage/>
+                    </FormItem>
+                 )}
+               />
 
-    <div>page</div>
+                <FormField
+                 control={form.control}
+                 name="password"
+                 render={({field}) => (
+                    <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                            <Input type="password" {...field}/>
+                        </FormControl>
+                        <FormMessage/>
+                    </FormItem>
+                 )}
+               />
+
+                <Button className='w-full' type="submit">Sign In</Button>
+            </form>
+         </Form>
+           <div className="text-center mt-4">
+          <p>
+           Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+        </div>
+    </div>
   )
 }
 
