c49c945345ece67124c51eebb6320505fcd74157
Ayush kumar <kumarayush042004@gmail.com>
2025-07-06 02:34:49 +0530

    integrated nodemailer and removed resend

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,7 +1,8 @@
-import {resend} from "@/lib/resend";
 
+import { transporter } from "@/lib/nodemailer";
 import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
+import {render} from "@react-email/render";
 
 
 export async function sendVerificationEmail(
@@ -9,14 +10,20 @@ export async function sendVerificationEmail(
     username: string,
     verifyCode: string
 ): Promise<ApiResponse>{
+   
     try {
-        await resend.emails.send({
-              from: 'Acme <onboarding@resend.dev>',
-              to: email,
-              subject: 'Mystery Message Verification Code',
-              react:VerificationEmail({ username, otp: verifyCode }),
-        })
-       return { success: true, message: 'Verification email sent successfully.' };
+    const html = await render(VerificationEmail({ username, otp: verifyCode }));
+
+    const mailOptions = {
+      from: `"Mystery Message" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Verify your Mystery Message account",
+      html,
+    };
+
+     await transporter.sendMail(mailOptions);
+     console.log(`📤 Email sent to ${email} with code ${verifyCode}`);
+    return { success: true, message: "Email sent successfully." };
         
     } catch (Error) {
          console.error('Error sending verification email:', Error);
