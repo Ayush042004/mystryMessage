0566edaf147721e293d0d557e5fb7b610c035793
Ayush kumar <kumarayush042004@gmail.com>
2025-07-06 12:25:23 +0530

    1

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,7 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
-import { ApiResponse } from "@/types/ApiResponse";
+
 
 
 export async function POST(request: Request){
